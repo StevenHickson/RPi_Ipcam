5e451bc8e44c681b97da91abbcf34b0c7a17fca3
Steven Hickson <me@stevenhickson.com>
2020-12-12 11:58:31 -0800

    Updated for python3

--- mjpg_serve.py
+++ mjpg_serve.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
 '''
 	orig author: Igor Maculan - n3wtron@gmail.com
 	A Simple mjpg stream http server
 '''
 import cv2
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler,HTTPServer
 import time
 
 capture=None
@@ -18,7 +17,7 @@ def diffImg(t0, t1, t2):
 
 class CamHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print self.path
+        print(self.path)
         if self.path.endswith('.mjpg'):
             self.send_response(200)
             self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
@@ -42,12 +41,12 @@ class CamHandler(BaseHTTPRequestHandler):
                     #cv2.circle(img, (320,240),100,255,-1)
                     #imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
                     r, buf = cv2.imencode(".jpg",img)
-                    self.wfile.write("--jpgboundary\r\n")
+                    self.wfile.write(bytes("--jpgboundary\r\n",'utf-8'))
                     self.send_header('Content-type','image/jpeg')
                     self.send_header('Content-length',str(len(buf)))
                     self.end_headers()
                     self.wfile.write(bytearray(buf))
-                    self.wfile.write('\r\n')
+                    self.wfile.write(bytes('\r\n', 'utf-8'))
                     time.sleep(0.05)
                 except KeyboardInterrupt:
                     break
@@ -56,19 +55,19 @@ class CamHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
-            self.wfile.write('<html><head></head><body>')
-            self.wfile.write('<img src="./cam.mjpg" />')
-            self.wfile.write('</body></html>')
+            self.wfile.write(bytes('<html><head></head><body>', 'utf-8'))
+            self.wfile.write(bytes('<img src="./cam.mjpg" />', 'utf-8'))
+            self.wfile.write(bytes('</body></html>', 'utf-8'))
             return
 
 def main():
     global capture
     capture = cv2.VideoCapture(0)
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640); 
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480);
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1080); 
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720);
     try:
-        server = HTTPServer(('',8080),CamHandler)
-        print "server started"
+        server = HTTPServer(('0.0.0.0',8080),CamHandler)
+        print("server started")
         server.serve_forever()
     except KeyboardInterrupt:
         capture.release()
